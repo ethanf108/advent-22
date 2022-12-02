7c3568c57f775fb43139fd49879189c249fab47c
Ethan Ferguson <ethanf108@gmail.com>
2022-12-02 13:52:53 -0500

    Advent 2 Scheme

--- scheme/utils.scm
+++ scheme/utils.scm
@@ -1,10 +1,10 @@
 (define (println . args) (if (null? args) (newline) (begin (display (car args)) (apply println (cdr args)))))
 
 (define (readlines filename)
-(call-with-input-file filename
-    (lambda (p)
-        (let loop ((line (read-line p))
-            (result '()))
-        (if (eof-object? line)
-            (reverse result)
-            (loop (read-line p) (cons line result)))))))
+    (call-with-input-file filename
+        (lambda (p)
+            (let loop ((line (read-line p))
+                (result '()))
+            (if (eof-object? line)
+                (reverse result)
+                (loop (read-line p) (cons line result)))))))
